b95e690989a46182fe0cd3fe22fd96b212fb77d9
JontePonte <john_nordstrand@hotmail.com>
2024-11-22 18:28:39 +0100

    Added relevant files

---
+++ ai_contenders/ai_super_aggressor.py
@@ -0,0 +1,25 @@
+import inspect
+
+
+def super_aggressor(game_info):
+    """ AI that allways tries to attack """
+
+    # Unpack game info
+    my_score, my_health, my_moves = None, None, None
+    opponent_score, opponent_health, opponent_moves = None, None, None
+    
+    for ai_name in game_info:
+        if ai_name == inspect.currentframe().f_code.co_name:
+            my_score = game_info[ai_name]['score']
+            my_health = game_info[ai_name]['health']
+            my_moves = game_info[ai_name]['moves']
+        else:
+            opponent_score = game_info[ai_name]['score']
+            opponent_health = game_info[ai_name]['health']
+            opponent_moves = game_info[ai_name]['moves']
+
+    action = "attack"
+    if my_health == 1:
+        action = "defend"
+
+    return action
---
+++ registry.py
@@ -0,0 +1,21 @@
+"""
+Add new AI:s here
+"""
+
+from ai_contenders.ai_full_random import full_random
+from ai_contenders.ai_aggressor import aggressor
+from ai_contenders.ai_super_aggressor import super_aggressor
+from ai_contenders.ai_super_defender import super_defender
+from ai_contenders.ai_advanced import advanced
+from ai_contenders.ai_smart_random import smart_random
+
+# All condenders are added here
+
+ai_list = [
+    full_random,
+    aggressor,
+    # super_aggressor,
+    # super_defender,
+    # advanced,
+    # smart_random,
+]
